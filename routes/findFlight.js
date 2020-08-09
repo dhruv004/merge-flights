519b7cf3aaab1ef8122ef488c1c3688fb6adb29e
Dhruv <datt.dhruv@gmail.com>
2020-08-09 20:04:49 +0200

    Delete findFlight.js (#1)
    deleted files

--- routes/findFlight.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var findFlightController = require('../controllers/findFlight');
-
-router.get('/', findFlightController.merge_sources);
-
-module.exports = router;
